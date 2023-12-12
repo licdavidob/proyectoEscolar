c7d6ab8dd8bd7bdbb56702242f8c2aa4e814aedd
David Olvera <licdavidob@gmail.com>
2023-12-12 10:00:47 -0600

    Manteniendo sólo los archivos a analizar

--- imss/SREFSMI_2023/libs/js/funcion_menu.js
+++
@@ -1,5 +0,0 @@
-$ (function (){
-	$("#menu_ctim").click(function(){
-		location.href="http://172.25.0.32/ctim/index.php";
-	});
-});
--- ss/SREFSMI_2023/fpdf/tutorial/makefont.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Generation of font definition file for tutorial 7
-require('../../../Copia de dpa/fpdf/font/makefont/makefont.php');
-
-MakeFont('calligra.ttf','calligra.afm');
-?>
